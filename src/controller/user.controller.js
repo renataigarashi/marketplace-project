2c77077686f5bafb605c73e027736a4ccee6d4ae
Renata Igarashi <rsigarashi@hotmail.com>
2024-02-14 20:46:03 -0300

    feat: implement controller

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,72 +1,103 @@
+const userService = require('../service/user.service');
 const findUserByIdController = async (req, res) => {
   try {
-  } catch (error) {
-    console.log('error: ', error);
+    const user = await userService.findUserByIdService(req.params.id);
+
+    if (!user) {
+      return res.status(404).send({ message: 'user not found' });
+    }
+
+    res.status(200).send(user);
+  } catch (err) {
+    if (err.kind == 'ObjectId') {
+      console.log(err.kind);
+      return res.status(400).send({ message: `Incorrect ID, try again` });
+    }
+    console.log('err: ', err.message);
     return res.status(500).send({ message: `Unexpected error, try again!` });
   }
 };
 
 const findAllUserController = async (req, res) => {
   try {
-  } catch (error) {
-    console.log('error: ', error);
-    return res.status(500).send({ message: `Unexpected error, try again!` });
+    return res.status(200).send(await userService.findAllUserService());
+  } catch (err) {
+    console.log('err: ', err);
+    return res.status(500).send({ message: `Unexpected err, try again!` });
   }
 };
 
 const createUserController = async (req, res) => {
   try {
-  } catch (error) {
-    console.log('error: ', error);
-    return res.status(500).send({ message: `Unexpected error, try again!` });
+    const body = req.body;
+
+    if (!body.name) {
+      return res.status(400).send({ message: `Field 'name' must be filled` });
+    }
+    return res.status(201).send(await userService.createUserService(body));
+  } catch (err) {
+    console.log('err: ', err);
+    return res.status(500).send({ message: `Unexpected err, try again!` });
   }
 };
 
 const updateUserController = async (req, res) => {
   try {
-  } catch (error) {
-    console.log('error: ', error);
-    return res.status(500).send({ message: `Unexpected error, try again!` });
+    const body = req.body;
+
+    if (!body.name) {
+      return res.status(400).send({ message: `Field 'name' must be filled` });
+    }
+    return res.send(await userService.updateUserService(req.params.id, body));
+  } catch (err) {
+    console.log('err: ', err);
+    return res.status(500).send({ message: `Unexpected err, try again!` });
   }
 };
 
 const removeUserController = async (req, res) => {
   try {
-  } catch (error) {
-    console.log('error: ', error);
-    return res.status(500).send({ message: `Unexpected error, try again!` });
+    const deleteUser = await userService.removeUserService(req.params.id);
+    if (deleteUser.deletedCount > 0) {
+      return res.status(200).send({ message: `User deleted sucessfully!` });
+    } else {
+      return res.status(404).send({ message: `User not founs, try again!` });
+    }
+  } catch (err) {
+    console.log('err: ', err);
+    return res.status(500).send({ message: `Unexpected err, try again!` });
   }
 };
 
 const addUserAddressController = async (req, res) => {
   try {
-  } catch (error) {
-    console.log('error: ', error);
-    return res.status(500).send({ message: `Unexpected error, try again!` });
+  } catch (err) {
+    console.log('err: ', err);
+    return res.status(500).send({ message: `Unexpected err, try again!` });
   }
 };
 
 const removeUserAddressController = async (req, res) => {
   try {
-  } catch (error) {
-    console.log('error: ', error);
-    return res.status(500).send({ message: `Unexpected error, try again!` });
+  } catch (err) {
+    console.log('err: ', err);
+    return res.status(500).send({ message: `Unexpected err, try again!` });
   }
 };
 
 const addUserFavProductController = async (req, res) => {
   try {
-  } catch (error) {
-    console.log('error: ', error);
-    return res.status(500).send({ message: `Unexpected error, try again!` });
+  } catch (err) {
+    console.log('err: ', err);
+    return res.status(500).send({ message: `Unexpected err, try again!` });
   }
 };
 
 const removeUserFavProductController = async (req, res) => {
   try {
-  } catch (error) {
-    console.log('error: ', error);
-    return res.status(500).send({ message: `Unexpected error, try again!` });
+  } catch (err) {
+    console.log('err: ', err);
+    return res.status(500).send({ message: `Unexpected err, try again!` });
   }
 };
 

d772c25df4827b5b3464c7d6091781b3be2a0794
Renata Igarashi <rsigarashi@hotmail.com>
2024-02-14 20:24:52 -0300

    Docs: comments routes

---
+++ src/controller/user.controller.js
@@ -0,0 +1,83 @@
+const findUserByIdController = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log('error: ', error);
+    return res.status(500).send({ message: `Unexpected error, try again!` });
+  }
+};
+
+const findAllUserController = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log('error: ', error);
+    return res.status(500).send({ message: `Unexpected error, try again!` });
+  }
+};
+
+const createUserController = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log('error: ', error);
+    return res.status(500).send({ message: `Unexpected error, try again!` });
+  }
+};
+
+const updateUserController = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log('error: ', error);
+    return res.status(500).send({ message: `Unexpected error, try again!` });
+  }
+};
+
+const removeUserController = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log('error: ', error);
+    return res.status(500).send({ message: `Unexpected error, try again!` });
+  }
+};
+
+const addUserAddressController = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log('error: ', error);
+    return res.status(500).send({ message: `Unexpected error, try again!` });
+  }
+};
+
+const removeUserAddressController = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log('error: ', error);
+    return res.status(500).send({ message: `Unexpected error, try again!` });
+  }
+};
+
+const addUserFavProduct = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log('error: ', error);
+    return res.status(500).send({ message: `Unexpected error, try again!` });
+  }
+};
+
+const removeUserFavProduct = async (req, res) => {
+  try {
+  } catch (error) {
+    console.log('error: ', error);
+    return res.status(500).send({ message: `Unexpected error, try again!` });
+  }
+};
+
+module.exports = {
+  findUserByIdController,
+  findAllUserController,
+  createUserController,
+  updateUserController,
+  removeUserController,
+  addUserAddressController,
+  removeUserAddressController,
+  addUserFavProduct,
+  removeUserFavProduct
+};
--- src/router/user.router.js
+++ src/router/user.router.js
@@ -2,17 +2,21 @@ const express = require('express');
 const router = express();
 const userController = require('../controller/user.controller');
 
-router.get('/findById/:id');
-router.get('/findAll');
+// GET
+router.get('/findById/:id', userController.findUserByIdController);
+router.get('/findAll', userController.findAllUserController);
 
-router.post('/create');
-router.post('/addAddress/:id');
-router.post('/addFavProduct/:id');
+// POST
+router.post('/create', userController.createUserController);
+router.post('/addAddress/:id', userController.addUserAddressController);
+router.post('/addFavProduct/:id', userController.addUserFavProduct);
 
-router.put('/update/:id');
+// PUT
+router.put('/update/:id', userController.updateUserController);
 
-router.delete('/remove/:id');
-router.delete('/removeAddress');
-router.delete('/removeFavProduct');
+// DELETE
+router.delete('/remove/:id', userController.removeUserController);
+router.delete('/removeAddress', userController.removeUserAddressController);
+router.delete('/removeFavProduct', userController.removeUserFavProduct);
 
 module.exports = router;

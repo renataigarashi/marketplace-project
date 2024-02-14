6fd023b65db6b9aeb0b18d8eca2fec9f71382ffa
Renata Igarashi <rsigarashi@hotmail.com>
2024-02-14 20:14:28 -0300

    feat: user route and controller

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 const express = require('express');
 const connectToDatabase = require('./src/database/database');
+const user = require('./src/router/user.router');
 
 const app = express();
 const port = 3000;
 
 app.use(express.json());
 connectToDatabase();
+
+app.use('/user', user);
 
 app.get('/', (req, res) => res.send({ message: 'Bem vindo ao marketplace' }));
 
